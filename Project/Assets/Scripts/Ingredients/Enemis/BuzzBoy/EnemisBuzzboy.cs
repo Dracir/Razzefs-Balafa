3f1f70b17e302af01763a0dd3c8e4474e7585c7f
richard <rail.rich@gmail.com>
2015-04-22 17:37:03 -0400

    Buzz

---
+++ Project/Assets/Scripts/Ingredients/Enemis/BuzzBoy/EnemisBuzzboy.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Magicolo;
+
+public class EnemisBuzzboy : StateLayer {
+	
+    StateMachine Machine {
+    	get { return ((StateMachine)machine); }
+    }
+	
+	public bool stationnary;
+	
+	public override void OnEnter() {
+		base.OnEnter();
+		
+	}
+	
+	public override void OnExit() {
+		base.OnExit();
+		
+	}
+	
+	public override void OnUpdate() {
+		base.OnUpdate();
+		
+	}
+}
