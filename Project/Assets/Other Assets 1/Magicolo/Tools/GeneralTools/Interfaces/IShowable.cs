bca436184ef984c9b9ec1fd0b907927df892cd95
Magicolo <straty91@hotmail.com>
2015-04-22 17:36:40 -0400

    aspoid

--- Project/Assets/Other Assets 1/Magicolo/Tools/GeneralTools/Interfaces/IShowable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace Magicolo {
-	public interface IShowable {
-
-		bool Showing { get; set; }
-	}
-}
---
+++ Project/Assets/Scripts/Character/Temperature/CharacterTemperatureBlazing.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Magicolo;
+
+public class CharacterTemperatureBlazing : State {
+	
+	CharacterTemperature Layer {
+		get { return ((CharacterTemperature)layer); }
+	}
+    
+	StateMachine Machine {
+		get { return ((StateMachine)machine); }
+	}
+	
+	public override void OnUpdate() {
+		base.OnUpdate();
+		
+		if (Layer.CurrentTemperature < Layer.blazingThreshold) {
+			SwitchState<CharacterTemperatureIdle>();
+			return;
+		}
+	}
+}
