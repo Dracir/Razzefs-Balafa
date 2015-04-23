96771c45147a34379119b300219a2596a32826ed
Magicolo <straty91@hotmail.com>
2015-04-22 20:12:46 -0400

    hhgghj

--- Project/Assets/Scripts/Temperature.cs
+++ Project/Assets/Scripts/Temperature.cs
@@ -6,5 +6,38 @@ using Magicolo;
 public class Temperature : MonoBehaviourExtended {
 
 	[Range(-1, 1)] public float temperature;
+	[Range(-1, 0)] public float freezingThreshold = 0.75F;
+	[Range(0, 1)] public float blazingThreshold = 0.75F;
+	[Min] public float resistance = 1;
+	
+	public bool IsFreezing {
+		get {
+			return temperature <= freezingThreshold;
+		}
+	}
+	
+	public bool IsBlazing {
+		get {
+			return temperature >= blazingThreshold;
+		}
+	}
+	
+	public float AmbientTemperature {
+		get {
+			return 0;
+		}
+	}
+	
+	void Update() {
+		float difference = AmbientTemperature - temperature;
+		float increment = difference * Time.deltaTime / resistance;
+		
+		if (increment > difference) {
+			temperature += increment;
+		}
+		else {
+			temperature = AmbientTemperature;
+		}
+	}
 }
 
