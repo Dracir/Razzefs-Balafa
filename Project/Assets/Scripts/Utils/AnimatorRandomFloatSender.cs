64aae8e8fa02315c9e0edea4424b35907639cf4c
Super Awesome Lounge Tower <a@a.com>
2015-04-24 16:38:44 -0400

    a

---
+++ Project/Assets/Scripts/Utils/AnimatorRandomFloatSender.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorRandomFloatSender : MonoBehaviour {
+
+	Animator animator;
+	
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+		animator.SetFloat("Random", Random.Range(0,1));
+	}
+}
