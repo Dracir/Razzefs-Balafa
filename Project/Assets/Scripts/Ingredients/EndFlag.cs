c4a79cc7dced838e723f132dc6e65da80409d084
richard <rail.rich@gmail.com>
2015-04-23 21:47:55 -0400

    as

---
+++ Project/Assets/Scripts/Ingredients/EndFlag.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndFlag : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player"){
+			Game.instance.SwitchState<GameNextLevel>();
+		}
+    }
+}
