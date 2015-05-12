b3eee68aa46bca77b727764de057f6af0cfc2b0a
Super Awesome Lounge Tower <a@a.com>
2015-05-12 12:31:43 -0400

    Adding the elder sign!
    Signed-off-by: Super Awesome Lounge Tower <a@a.com>

--- Project/Assets/Scripts/Ingredients/EndFlag.cs
+++ Project/Assets/Scripts/Ingredients/EndFlag.cs
@@ -6,6 +6,7 @@ public class EndFlag : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Player"){
 			Game.instance.SwitchState<GameNextLevel>();
+		Debug.Log("I'm making it happen");
 		}
     }
 }
