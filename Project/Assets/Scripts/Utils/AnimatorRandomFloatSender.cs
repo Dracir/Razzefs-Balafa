532050065a5f3efe0fdef5758ca24d92425ad1bb
richard <rail.rich@gmail.com>
2015-05-20 02:48:53 -0400

    Separated the fire flames animation Flamme dis-a-pear when fire is frozen

--- Project/Assets/Scripts/Utils/AnimatorRandomFloatSender.cs
+++ Project/Assets/Scripts/Utils/AnimatorRandomFloatSender.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class AnimatorRandomFloatSender : MonoBehaviour {
 
 	Animator animator;
+	public string[] animationString = new string[]{"Random"};
 	
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -11,6 +12,8 @@ public class AnimatorRandomFloatSender : MonoBehaviour {
 	
 	
 	void Update () {
-		animator.SetFloat("Random", Random.Range(0f,1f));
+		foreach (var str in animationString) {
+			animator.SetFloat(str, Random.Range(0f,1f));
+		}
 	}
 }
